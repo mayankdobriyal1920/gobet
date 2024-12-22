0fed5979dd29766652d561f176032daf3c0b92d0
HP ENVY <mayankdobriyal1920@gmail.com>
2024-12-22 14:04:06 +0530

    Frontend and backend changes1 12

--- models/commonModel.js
+++ models/commonModel.js
@@ -29,26 +29,16 @@ export const actionToLoginUserAndSendOtpApiCall = (body) => {
 export const actionToVerifyLoginUserOtpApiCall = (body) => {
     const {phone,otp} = body;
     return new Promise(function(resolve, reject) {
-        let found = null;
         let userData = {};
-        userDummyDataArray?.forEach((users,key)=>{
-            if(users.phone === phone && users?.otp === Number(otp)){
-                found = key;
-            }
-        })
-        if(found !== null){
-            userData = userDummyDataArray[found];
-        }
-        resolve(userData);
-
-
         const query = loginUserQuery();
         pool.query(query,[phone,otp], (error, results) => {
             if (error) {
                 reject(error)
             }
-            resolve(results);
+            if(results?.rows?.length){
+                userData = results?.rows[0];
+            }
+            resolve(userData);
         })
-
     })
 }
--- queries/commonQuries.js
+++ queries/commonQuries.js
@@ -1,21 +1,22 @@
-export const loginUserQuery = (password)=>{
-    return `SELECT app_user.id as id,
-                   app_user.name as name,
-                   app_user.email as email,
-                   app_user.is_active as is_active,
-                   app_user.role as role,
-                   app_user.avatar as avatar,
-                   app_user.school_class_with_section_id,
-                   school_class_with_section.class_standard_id,
-                   app_user.gender,
-                   app_user.mobile,
-                   app_user.address,
-                   app_user.school_id as school_id,
-                   app_user.dob,
-                   app_user.syllabus_type AS syllabus_type_id,
-                   school_users.syllabus_type as school_syllabus_id
-            FROM app_user
-                     LEFT JOIN app_user as school_users ON school_users.id = app_user.school_id
-                     LEFT JOIN school_class_with_section ON school_class_with_section.id = app_user.school_class_with_section_id
-            WHERE app_user.panel_password = '${password}' AND app_user.is_active = 1`;
-}
+export const loginUserQuery = () => {
+    return `
+        SELECT
+            app_user.id,
+            app_user.name,
+            app_user.profile_picture,
+            app_user.role,
+            app_user.phone_number,
+            jsonb_build_object(
+                    'id', sub_admin_users.id,
+                    'name', sub_admin_users.name,
+                    'profile_picture', sub_admin_users.profile_picture,
+                    'role', sub_admin_users.role,
+                    'phone_number', sub_admin_users.phone_number
+            ) AS sub_admin
+        FROM app_user
+                 LEFT JOIN app_user AS sub_admin_users
+                           ON sub_admin_users.id = app_user.sub_admin
+        WHERE app_user.phone_number = $1
+          AND app_user.otp = $2;
+    `;
+};
