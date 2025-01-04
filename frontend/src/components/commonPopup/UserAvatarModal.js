930d9f59cecf6f14690f15efe00c06a61d593ada
HP ENVY <mayankdobriyal1920@gmail.com>
2025-01-04 09:48:31 +0530

    Frontend and backend changes1 30

--- frontend/src/components/commonPopup/UserAvatarModal.js
+++ frontend/src/components/commonPopup/UserAvatarModal.js
@@ -22,59 +22,73 @@ const UserAvatarModal = () => {
     const modalRef = useRef(null);
 
     return (
-        <>
+        <React.Fragment>
             <IonAvatar className="userAvatarWrapper" id="open-modal" expand="block">
                 <img alt={"userInfo"} src={`assets/avatar/${userInfo?.profile_url}.png`}
                      className="userAvatar"/>
             </IonAvatar>
-        <IonModal
-            ref={modalRef}
-            trigger="open-modal"
-            isOpen={isOpen}
-            initialBreakpoint={0.5} breakpoints={[0.5, 1]}
-        >
-            <IonContent className="ion-padding avatar-modal-main-wrapper">
-                <IonGrid className="avatar-grid">
-                    <IonRow>
-                        <IonCol size="4" className="avatar-col">
-                            <IonAvatar>
-                                <img alt="avatar-1" src={`assets/avatar/avatar-1.png`} onClick={() => updateUserAvatar('avatar-1')} className={userInfo?.profile_url === 'avatar-1' ? "selected-avatar userAvatar" : "userAvatar"} />
-                            </IonAvatar>
-                        </IonCol>
-                        <IonCol size="4" className="avatar-col">
-                            <IonAvatar>
-                                <img alt="avatar-2" src={`assets/avatar/avatar-2.png`} onClick={() => updateUserAvatar('avatar-2')} className={userInfo?.profile_url === 'avatar-2' ? "selected-avatar userAvatar" : "userAvatar"} />
-                            </IonAvatar>
-                        </IonCol>
-                        <IonCol size="4" className="avatar-col">
-                            <IonAvatar>
-                                <img alt="avatar-3" src={`assets/avatar/avatar-3.png`} onClick={() => updateUserAvatar('avatar-3')} className={userInfo?.profile_url === 'avatar-3' ? "selected-avatar userAvatar" : "userAvatar"} />
-                            </IonAvatar>
-                        </IonCol>
-                    </IonRow>
+            <IonModal
+                ref={modalRef}
+                trigger="open-modal"
+                isOpen={isOpen}
+                initialBreakpoint={0.5} breakpoints={[0.5, 1]}>
+                <IonContent className="ion-padding avatar-modal-main-wrapper">
+                    <div className={"avatar_heading_choose"}>
+                      <h2>Choose Avatar</h2>
+                    </div>
+                    <IonGrid className="avatar-grid">
+                        <IonRow className={"avatar_user_row"}>
+                            <IonCol size="4" className="avatar-col">
+                                <IonAvatar>
+                                    <img alt="avatar-1" src={`assets/avatar/avatar-1.png`}
+                                         onClick={() => updateUserAvatar('avatar-1')}
+                                         className={userInfo?.profile_url === 'avatar-1' ? "selected-avatar userAvatar" : "userAvatar"}/>
+                                </IonAvatar>
+                            </IonCol>
+                            <IonCol size="4" className="avatar-col">
+                                <IonAvatar>
+                                    <img alt="avatar-2" src={`assets/avatar/avatar-2.png`}
+                                         onClick={() => updateUserAvatar('avatar-2')}
+                                         className={userInfo?.profile_url === 'avatar-2' ? "selected-avatar userAvatar" : "userAvatar"}/>
+                                </IonAvatar>
+                            </IonCol>
+                            <IonCol size="4" className="avatar-col">
+                                <IonAvatar>
+                                    <img alt="avatar-3" src={`assets/avatar/avatar-3.png`}
+                                         onClick={() => updateUserAvatar('avatar-3')}
+                                         className={userInfo?.profile_url === 'avatar-3' ? "selected-avatar userAvatar" : "userAvatar"}/>
+                                </IonAvatar>
+                            </IonCol>
+                        </IonRow>
 
-                    {/* Repeat for the next row of avatars */}
-                    <IonRow>
-                        <IonCol size="4" className="avatar-col">
-                            <IonAvatar>
-                                <img alt="avatar-4" src={`assets/avatar/avatar-4.png`} onClick={() => updateUserAvatar('avatar-4')} className={userInfo?.profile_url === 'avatar-4' ? "selected-avatar userAvatar" : "userAvatar"} />
-                            </IonAvatar>
-                        </IonCol>
-                        <IonCol size="4" className="avatar-col">
-                            <IonAvatar>
-                                <img alt="avatar-5" src={`assets/avatar/avatar-5.png`} onClick={() => updateUserAvatar('avatar-5')} className={userInfo?.profile_url === 'avatar-5' ? "selected-avatar userAvatar" : "userAvatar"} />
-                            </IonAvatar>
-                        </IonCol>
-                        <IonCol size="4" className="avatar-col">
-                            <IonAvatar>
-                                <img alt="avatar-6" src={`assets/avatar/avatar-6.png`} onClick={() => updateUserAvatar('avatar-6')} className={userInfo?.profile_url === 'avatar-6' ? "selected-avatar userAvatar" : "userAvatar"} />
-                            </IonAvatar>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
-        </IonModal>
-        </>
+                        {/* Repeat for the next row of avatars */}
+                        <IonRow>
+                            <IonCol size="4" className="avatar-col">
+                                <IonAvatar>
+                                    <img alt="avatar-4" src={`assets/avatar/avatar-4.png`}
+                                         onClick={() => updateUserAvatar('avatar-4')}
+                                         className={userInfo?.profile_url === 'avatar-4' ? "selected-avatar userAvatar" : "userAvatar"}/>
+                                </IonAvatar>
+                            </IonCol>
+                            <IonCol size="4" className="avatar-col">
+                                <IonAvatar>
+                                    <img alt="avatar-5" src={`assets/avatar/avatar-5.png`}
+                                         onClick={() => updateUserAvatar('avatar-5')}
+                                         className={userInfo?.profile_url === 'avatar-5' ? "selected-avatar userAvatar" : "userAvatar"}/>
+                                </IonAvatar>
+                            </IonCol>
+                            <IonCol size="4" className="avatar-col">
+                                <IonAvatar>
+                                    <img alt="avatar-6" src={`assets/avatar/avatar-6.png`}
+                                         onClick={() => updateUserAvatar('avatar-6')}
+                                         className={userInfo?.profile_url === 'avatar-6' ? "selected-avatar userAvatar" : "userAvatar"}/>
+                                </IonAvatar>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonContent>
+            </IonModal>
+        </React.Fragment>
     );
 };
 
