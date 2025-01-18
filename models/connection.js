bdef454edf5af422fb693a6cbba341eaa8cdb4b4
HP ENVY <mayankdobriyal1920@gmail.com>
2025-01-18 10:07:27 +0530

    Frontend and backend changes 73

--- models/connection.js
+++ models/connection.js
@@ -6,18 +6,18 @@ dotenv.config();
 const dbConfig =
     process.env.NODE_ENV === 'PRODUCTION' ?
     {
-        host: 'dpg-ctjrh70gph6c738ildn0-a',
+        host: 'db.uupimsesrmizjiwvoiit.supabase.co',
         port: 5432,
-        user: 'getbet', // PostgreSQL uses 'user' instead of 'username'
-        password: 'RHaMdjZVq1vfoK5XxgGjVwAJGlGiZPZw',
-        database: 'getbet',
+        user: 'postgres', // PostgreSQL uses 'user' instead of 'username'
+        password: '2!@32Ar*tHb2JQ2',
+        database: 'postgres',
         max: 10, // Maximum number of connections in the pool
         idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
         connectionTimeoutMillis: 2000, // Return an error if connection takes more than 2 seconds
     }
     :
     {
-        connectionString: 'postgres://getbet:RHaMdjZVq1vfoK5XxgGjVwAJGlGiZPZw@dpg-ctjrh70gph6c738ildn0-a.oregon-postgres.render.com:5432/getbet?sslmode=require',
+        connectionString: 'postgresql://postgres:2!@32Ar*tHb2JQ2@db.uupimsesrmizjiwvoiit.supabase.co:5432/postgres',
         connectionTimeoutMillis: 5000,  // Timeout in milliseconds
         idleTimeoutMillis: 30000,       // Idle connection timeout
         query_timeout: 60000            // Query timeout
